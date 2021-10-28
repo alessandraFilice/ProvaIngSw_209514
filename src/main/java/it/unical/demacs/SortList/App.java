40c16ac3854d59239813998bedd3f420c85bff52
Alessandra Filice <flclsn00p65h501e@studenti.unical.it>
2021-10-28 09:02:07 +0200

    First commit

---
+++ src/main/java/it/unical/demacs/SortList/App.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.SortList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
